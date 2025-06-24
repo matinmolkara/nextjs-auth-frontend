3a4443ba27f41f71290843564bb80abbfac9ade7
@matinmolkara <zahramolkara@gmail.com>
2025-06-24 23:41:41 +0330

    layout change

--- app/(admin)/admin-panel/layout.jsx
+++ app/(admin)/admin-panel/layout.jsx
@@ -2,8 +2,8 @@
 import ProtectedRoute from "@/components/ProtectedRoute";
 export default function AdminLayout({ children }) {
   return (
-    <div className="admin-layout">
+ 
       <ProtectedRoute adminOnly>{children}</ProtectedRoute>
-    </div>
+    
   );
 }
