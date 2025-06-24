4ccc51ba5230510a3d38aa03cb2c904c1ddb333c
@matinmolkara <zahramolkara@gmail.com>
2025-06-24 21:59:11 +0330

    remove redirection and token check from middleware

--- middleware.js
+++ middleware.js
@@ -1,17 +1,17 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function middleware(req) {
-const cookieStore = await cookies(); // ← اینجا await اضافه شد
-const token = cookieStore.get("token")?.value;
+// export async function middleware(req) {
+// const cookieStore = await cookies(); // ← اینجا await اضافه شد
+// const token = cookieStore.get("token")?.value;
 
-  if (!token) {
-    const loginUrl = new URL("/users/login", req.url);
-    return NextResponse.redirect(loginUrl);
-  }
+//   if (!token) {
+//     const loginUrl = new URL("/users/login", req.url);
+//     return NextResponse.redirect(loginUrl);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: ["/dashboard", "/profile/:path*", "/admin-panel/:path*"],
