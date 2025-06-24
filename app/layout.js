e70e207e35374e9cab8f9d9374fe2475f45f2944
@matinmolkara <zahramolkara@gmail.com>
2025-06-25 00:00:39 +0330

    adding children to protected route

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa">
       <body>
-        <ClientLayout>{children}</ClientLayout>{" "}
-        {/* اینجا از کامپوننت کلاینتی استفاده می‌کنیم */}
+        <ClientLayout>{children}</ClientLayout>
+      
         <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></Script>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></Script>
       </body>
--- components/ProtectedRoute.jsx
+++ components/ProtectedRoute.jsx
@@ -3,39 +3,39 @@ import { useAuth } from "@/context/authContext";
 import { useRouter, usePathname } from "next/navigation";
 import React ,{useEffect} from "react";
 
-const ProtectedRoute = () => {
-    const { user } = useAuth();
-    const router = useRouter();
-    const pathname = usePathname();
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  const router = useRouter();
+  const pathname = usePathname();
 
-    useEffect(() => {
-      // تا وقتی user لود نشده هیچ کاری نکن
-      if (user === null) return;
+  useEffect(() => {
+    // تا وقتی user لود نشده هیچ کاری نکن
+    if (user === null) return;
 
-      // کاربر لاگین نیست
-      if (!user) {
-        router.push("/users/login");
-      }
+    // کاربر لاگین نیست
+    if (!user) {
+      router.push("/users/login");
+    }
 
-      // اگر adminOnly هست و نقش کاربر admin نیست
-      else if (adminOnly && user.role !== "admin") {
-        router.push("/profile");
-      }
+    // اگر adminOnly هست و نقش کاربر admin نیست
+    else if (adminOnly && user.role !== "admin") {
+      router.push("/profile");
+    }
 
-      // اگر admin هست و داره مسیر اشتباه می‌ره (مثلاً به /profile)
-      else if (user.role === "admin" && pathname.startsWith("/profile")) {
-        router.push("/admin-panel/dashboard");
-      }
+    // اگر admin هست و داره مسیر اشتباه می‌ره (مثلاً به /profile)
+    else if (user.role === "admin" && pathname.startsWith("/profile")) {
+      router.push("/admin-panel/dashboard");
+    }
 
-      // اگر user هست و داره می‌ره تو مسیر admin-panel
-      else if (user.role === "user" && pathname.startsWith("/admin-panel")) {
-        router.push("/profile");
-      }
-    }, [user, pathname]);
+    // اگر user هست و داره می‌ره تو مسیر admin-panel
+    else if (user.role === "user" && pathname.startsWith("/admin-panel")) {
+      router.push("/profile");
+    }
+  }, [user, pathname]);
 
-    if (!user) return null;
+  if (!user) return null;
 
-    return children;
-}
+  return children;
+};
 
 export default ProtectedRoute
