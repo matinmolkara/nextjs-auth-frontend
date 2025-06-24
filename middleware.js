9b65dd70547c088919e687f117e05d2ea114c321
@matinmolkara <zahramolkara@gmail.com>
2025-06-24 22:24:37 +0330

    return back middleware

---
+++ middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  return NextResponse.redirect(new URL("/home", request.url));
+}
+// export async function middleware(req) {
+// const cookieStore = await cookies(); // ← اینجا await اضافه شد
+// const token = cookieStore.get("token")?.value;
+
+//   if (!token) {
+//     const loginUrl = new URL("/users/login", req.url);
+//     return NextResponse.redirect(loginUrl);
+//   }
+
+//   return NextResponse.next();
+// }
+
+export const config = {
+  matcher: ["/dashboard", "/profile/:path*", "/admin-panel/:path*"],
+};
