7b8e2ba350842bbb6da8f45d5e314d1db48cd6bf
@matinmolkara <zahramolkara@gmail.com>
2025-06-25 00:55:53 +0330

    protected route adding loading

--- components/ProtectedRoute.jsx
+++ components/ProtectedRoute.jsx
@@ -5,35 +5,36 @@ import React ,{useEffect} from "react";
 
 const ProtectedRoute = ({ children, adminOnly = false }) => {
   const { user } = useAuth();
-  const router = useRouter();
-  const pathname = usePathname();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // تا وقتی user لود نشده هیچ کاری نکن
     if (user === null) return;
 
-    // کاربر لاگین نیست
+    // کاربر غیر لاگین → بره لاگین
     if (!user) {
-      router.push("/users/login");
+      router.replace("/users/login");
     }
 
-    // اگر adminOnly هست و نقش کاربر admin نیست
-    else if (adminOnly && user.role !== "admin") {
-      router.push("/profile");
-    }
-
-    // اگر admin هست و داره مسیر اشتباه می‌ره (مثلاً به /profile)
-    else if (user.role === "admin" && pathname.startsWith("/profile")) {
-      router.push("/admin-panel/dashboard");
-    }
-
-    // اگر user هست و داره می‌ره تو مسیر admin-panel
+    // کاربر یوزر ولی مسیر ادمین اومده → بره profile
     else if (user.role === "user" && pathname.startsWith("/admin-panel")) {
-      router.push("/profile");
+      router.replace("/profile");
     }
+
+    // کاربر ادمین ولی مسیر profile اومده → بره داشبورد
+    else if (user.role === "admin" && pathname.startsWith("/profile")) {
+      router.replace("/admin-panel/dashboard");
+    }
+
+    setLoading(false);
   }, [user, pathname]);
 
-  if (!user) return null;
+  if (loading || !user) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "2rem" }}>
+        در حال بررسی سطح دسترسی...
+      </div>
+    );
+  }
 
   return children;
 };
