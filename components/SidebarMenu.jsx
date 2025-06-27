043e52892d62f2b8ee6bc7875bc1b70e950a4ed8
@matinmolkara <zahramolkara@gmail.com>
2025-06-27 22:58:22 +0330

    mobile menu links

--- components/SidebarMenu.jsx
+++ components/SidebarMenu.jsx
@@ -2,6 +2,7 @@
 "use client";
 import React, { useEffect, useRef, useState, useContext } from "react";
 import { useCartContext } from "@/context/cartContext";
+import { useAuth } from "@/context/authContext";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/components/MobileMenu.module.css";
@@ -9,6 +10,7 @@ import stylesHeader from "../styles/components/Header.module.css";
 import Icon from "../public/images/Icon.png";
 import Searchz from "../public/images/icon/searchz.png";
 const MobileMenu = ({ categories }) => {
+  const { user,logout } = useAuth(); 
   const { cartItems } = useCartContext();
   const [isActive, setIsActive] = useState(false);
   const [openSubmenus, setOpenSubmenus] = useState({});
@@ -85,12 +87,12 @@ const MobileMenu = ({ categories }) => {
   return (
     <>
       <div className="mobile-header" id="mobileHeader">
-      <div className="container">
+        <div className="container">
           <div className="row">
             <div className="col-3">
               <div className={stylesHeader.logo}>
                 <div className="d-flex align-content-center justify-content-end">
-                  <Link href="#">
+                  <Link href="/">
                     <Image className={styles.logoImage} src={Icon} alt="logo" />
                   </Link>
                 </div>
@@ -100,7 +102,30 @@ const MobileMenu = ({ categories }) => {
               <div className="d-flex justify-content-end">
                 <div className={stylesHeader.login}>
                   <i className="bi bi-person"></i>
-                  <Link href="#"> ورود به پنل کاربری </Link>
+                  {user ? (
+                    <form
+                      className="d-flex"
+                      onSubmit={async (e) => {
+                        e.preventDefault();
+                        await logout();
+                      }}
+                    >
+                      <Link href="/profile/userinfo"></Link>
+                      <Link
+                        href={
+                          user?.role === "admin"
+                            ? "/admin-panel/dashboard"
+                            : "/profile/userinfo"
+                        }
+                      >
+                        {user.name || user.email}
+                      </Link>
+                      <button>خروج</button>
+                    </form>
+                  ) : (
+                    <Link href="/users/login">ورود به پنل کاربری</Link>
+                  )}
+                  
                 </div>
                 <div className={stylesHeader.cart}>
                   <Link href="/checkout/cart">
@@ -113,33 +138,32 @@ const MobileMenu = ({ categories }) => {
             </div>
           </div>
           <div className="row">
-              <div className="col-9">
-                <div className={stylesHeader.wrap}>
-                  <div className={stylesHeader.search}>
-                    <input
-                      type="text"
-                      className={`${stylesHeader.searchTerm} p-3`}
-                      placeholder="جستجوی محصول..."
-                    />
-                    <button type="submit" className={stylesHeader.searchButton}>
-                      <Image src={Searchz} alt="search icon" />
-                    </button>
-                  </div>
+            <div className="col-9">
+              <div className={stylesHeader.wrap}>
+                <div className={stylesHeader.search}>
+                  <input
+                    type="text"
+                    className={`${stylesHeader.searchTerm} p-3`}
+                    placeholder="جستجوی محصول..."
+                  />
+                  <button type="submit" className={stylesHeader.searchButton}>
+                    <Image src={Searchz} alt="search icon" />
+                  </button>
                 </div>
               </div>
-              <div className="col-3 d-flex justify-content-end">
-                <button
-                  className="toggleHamburger menuToggle"
-                  onClick={toggleMenu}
-                  id="menuToggle"
-                >
-                  <i className="bi bi-grid-3x3-gap"></i>
-                </button>
-              </div>
+            </div>
+            <div className="col-3 d-flex justify-content-end">
+              <button
+                className="toggleHamburger menuToggle"
+                onClick={toggleMenu}
+                id="menuToggle"
+              >
+                <i className="bi bi-grid-3x3-gap"></i>
+              </button>
             </div>
           </div>
-
         </div>
+      </div>
 
       <div
         ref={menuRef}
@@ -147,11 +171,13 @@ const MobileMenu = ({ categories }) => {
       >
         <div className={styles.menuContent}>
           <Link href="/">
-          <i className="bi bi-house"></i>
-          خانه</Link>
+            <i className="bi bi-house"></i>
+            خانه
+          </Link>
           <Link href="/contactus">
-          <i className="bi bi-telephone"></i>
-          تماس با ما</Link>
+            <i className="bi bi-telephone"></i>
+            تماس با ما
+          </Link>
           {renderMenu(categories)}
         </div>
       </div>
